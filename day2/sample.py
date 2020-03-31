615d9bd58abd0f2276bfa75930dae69166c83606
Josh Medeski <josh@digitalcrafts.com>
2020-03-31 14:13:59 -0500

    Print name on sample

--- day2/sample.py
+++ day2/sample.py
@@ -2,12 +2,12 @@ first_name = input('What is your first name? ')
 last_name = input('What is your last name? ')
 middle_name = 'danger'
 
+print(first_name)
+print(middle_name)
+print(last_name)
+
 # "print" is a function called with "()"
 # print()
 
 # Argument goes inside the "()""
 # print('hello world')
-
-print(first_name)
-print(middle_name)
-print(last_name)
